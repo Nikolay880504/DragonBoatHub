794316fc77de42579e4d43f6ef3a559481399aee
NIKOLAY <stin541988@gmail.com>
2024-10-31 16:43:53 +0200

    Regestration bot

---
+++ DragonBot/DragonBot/Handlers/BotHandlers.cs
@@ -0,0 +1,50 @@
+﻿
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using Newtonsoft.Json.Linq;
+using Telegram.Bot.Types.Enums;
+using System;
+
+namespace DragonBot.Handlers
+{
+    internal class BotHandlers
+    {
+        internal static async Task HandleErrorAsync(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+
+        }
+
+        internal static async Task HandleUpdateAsync(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            var chat = update.Message.Chat;
+
+            switch (update.Type)
+            {
+
+                case UpdateType.Message:
+                case UpdateType.CallbackQuery:
+                    var messageText = update.Type == UpdateType.Message
+                        ? update.Message.Text
+                        : update.CallbackQuery.Data;
+
+                    switch (messageText)
+                    {
+                        case "/start":
+                            await client.SendTextMessageAsync(
+                                   chat.Id,
+                                  "/start");
+
+                            break;
+                    }
+
+
+                    break;
+
+                default:
+
+                    break;
+            }
+
+        }
+    }
+}
--- DragonBot/DragonBot/Program.cs
+++ DragonBot/DragonBot/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace DragonBot
+﻿using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+
+namespace DragonBot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static readonly string token;
+
+
+        static Program()
         {
-            
+            var configuration = new ConfigurationBuilder()
+               .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+               .Build();
+            token = configuration["BotSettings:Token"]!;
         }
+
+        static async Task Main(string[] args)
+        {
+            var botClient = new TelegramBotClient(token);
+
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = new[] { UpdateType.Message, UpdateType.CallbackQuery }, 
+                ThrowPendingUpdates = true 
+            };
+
+            botClient.StartReceiving(
+                Handlers.BotHandlers.HandleUpdateAsync,
+                Handlers.BotHandlers.HandleErrorAsync,
+                receiverOptions
+            );
+            await Task.Delay(-1);
+        }
+
+       
     }
+
 }
